a87d3667fc5872f9b3776e8222d0a26a344e4bb1
GhosttBatt <ghosttbatt@gmail.com>
2025-09-19 21:38:48 +0530

    seek.py

--- HeartBeat/modules/vcbot/seek.py
+++ HeartBeat/modules/vcbot/seek.py
@@ -10,7 +10,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 # Seek Stream (seek)
-@Client.on_message(filters.command("seek", ".") & SUDOERS)
+@Client.on_message(filters.command("seek", ".") & SUDO_USERS)
 async def seek_stream(client, message):
     chat_id = message.chat.id
     if len(message.command) < 2:
@@ -50,7 +50,7 @@ async def seek_stream(client, message):
         await eor(message, f"**Error:** `{e}`")
 
 # Seek Stream (cseek)
-@Client.on_message(filters.command("cseek", ".") & SUDOERS)
+@Client.on_message(filters.command("cseek", ".") & SUDO_USERS)
 async def seek_stream_(client, message):
     if not message.from_user:
         return await eor(message, "**🤖 This command cannot be used by anonymous users!**")
@@ -95,7 +95,7 @@ async def seek_stream_(client, message):
         await eor(message, f"**Error:** `{e}`")
 
 # Speed Stream (speed)
-@Client.on_message(filters.command("speed", ".") & SUDOERS)
+@Client.on_message(filters.command("speed", ".") & SUDO_USERS)
 async def speed_stream(client, message):
     chat_id = message.chat.id
     if len(message.command) < 2:
@@ -128,7 +128,7 @@ async def speed_stream(client, message):
         await eor(message, f"**Error:** `{e}`")
 
 # Speed Stream (cspeed)
-@Client.on_message(filters.command("cspeed", ".") & SUDOERS)
+@Client.on_message(filters.command("cspeed", ".") & SUDO_USERS)
 async def speed_stream_(client, message):
     if not message.from_user:
         return await eor(message, "**🤖 This command cannot be used by anonymous users!**")
@@ -166,7 +166,7 @@ async def speed_stream_(client, message):
         await eor(message, f"**Error:** `{e}`")
 
 # Bassboost Stream (bassboost)
-@Client.on_message(filters.command([bassboost", ".") & SUDOERS)
+@Client.on_message(filters.command([bassboost", ".") & SUDO_USERS)
 async def bassboost_stream(client, message):
     chat_id = message.chat.id
     if len(message.command) < 2:
@@ -198,7 +198,7 @@ async def bassboost_stream(client, message):
         await eor(message, f"**Error:** `{e}`")
 
 # Bassboost Stream (cbassboost)
-@Client.on_message(filters.command("cbassboost", ".") & SUDOERS)
+@Client.on_message(filters.command("cbassboost", ".") & SUDO_USERS)
 async def bassboost_stream_(client, message):
     if not message.from_user:
         return await eor(message, "**🤖 This command cannot be used by anonymous users!**")
