65bc0ba9316a788a7f577f2a499ae6a01951682d
GhosttBatt <ghosttbatt@gmail.com>
2025-09-20 21:08:01 +0530

    mention.py

--- HeartBeat/modules/basic/mention.py
+++ HeartBeat/modules/basic/mention.py
@@ -15,7 +15,7 @@ hmention = partial("<a href='tg://user?id={}'>\u200B</a>{}".format)
 @Client.on_message(filters.command("mention", ".") & filters.me)
 async def mention_user(bot: Client, message: Message):
     if len(message.command) < 3:
-        await message.edit("Incorrect format\nExample: .mention @Athfan CTO")
+        await message.edit("Incorrect format\nExample: .mention @GhosttBatt i ❤️ u")
         await asyncio.sleep(3)
         await message.delete()
         return
