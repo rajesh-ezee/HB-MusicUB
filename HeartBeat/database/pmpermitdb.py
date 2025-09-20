8cbcf23224674240cbd6f12a7c776312cf1fa3bb
GhosttBatt <ghosttbatt@gmail.com>
2025-09-20 23:38:34 +0530

    pmpermitdb.py

--- HeartBeat/database/pmpermitdb.py
+++ HeartBeat/database/pmpermitdb.py
@@ -4,16 +4,15 @@ import asyncio
 collection = cli["HeartBeat"]["pmpermit"]
 
 # Default PM Permit message
-PMPERMIT_MESSAGE = """**<blockquote>☆ . * ● ¸ . ✦ .★　° :. ★ * • ○ ° ★<blockquote>
+PMPERMIT_MESSAGE = """**<blockquote>☆ . * ● ¸ . ✦ .★° :. ★ * • ○ ° ★</blockquote>
 <blockquote>ʜᴇʏ ɪ'ᴍ 𝐇𝐞𝐚𝐫𝐭𝐁𝐞𝐚𝐭-✗-𝐁𝐨𝐭</blockquote>
-<blockquote>☆ . * ● ¸ . ✦ .★　° :. ★ * • ○ ° ★</blockquote>
-<blockquote>➽─────────────────❥</blockquote>
+<blockquote>➽───────────────❥</blockquote>
 <blockquote>💕 ᴛᴀɢ ᴍʏ ʟᴏᴠᴇ 🦋
  https://t.me/HeartBeat_Fam</blockquote>
-<blockquote>➽─────────────────❥</blockquote>
+<blockquote>➽───────────────❥</blockquote>
 <blockquote>😈 ᴏᴛʜᴇʀᴡɪꜱᴇ, ᴡᴀɪᴛ ᴜɴᴛɪʟ ᴍʏ ʙᴏꜱꜱ ᴄᴏᴍᴇꜱ, ᴅᴏɴ'ᴛ ꜱᴘᴀᴍ ᴍᴇ..
 ʏᴏᴜ ᴡɪʟʟ ʙᴇ ᴀᴜᴛᴏʙʟᴏᴄᴋ (ᴜᴘᴛᴏ 3 ᴍᴇꜱꜱᴀɢᴇꜱ)</blockquote>
-<blockquote>☆ . * ● ¸ . ✦ .★　° :. ★ * • ○ ° ★</blockquote>**"""
+<blockquote>☆ . * ● ¸ . ✦ .★° :. ★ * • ○ ° ★</blockquote>**"""
 
 # Default block message
 BLOCKED = "**<blockquote>ʙᴇᴇᴘ ʙᴏᴏᴘ ꜰᴏᴜɴᴅᴇᴅ ᴀ ꜱᴘᴀᴍᴍᴇʀ!, ʙʟᴏᴄᴋᴇᴅ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ!</blockquote>**"
